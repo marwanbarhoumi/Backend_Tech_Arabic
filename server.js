1775a2ae027f6e8b5ac727c9b1470639e95824fa
marwan2 <marwanbarhou77@gmail.oom.com>
2026-01-25 21:10:29 +0100

    update

--- server.js
+++ server.js
@@ -55,4 +55,6 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
+  console.log("ELEVEN FROM SPELLING ROUTE:", process.env.ELEVENLABS_API_KEY);
+
 });
